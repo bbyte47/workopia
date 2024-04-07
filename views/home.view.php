78899d5a61c630bb378bac2c2290c3639cecf36c
bbyte47 <gregg@bari-flora.com>
2024-04-07 09:11:08 -0400

    fixed home view

--- views/home.view.php
+++ views/home.view.php
@@ -2,7 +2,8 @@
 <?php loadPartial('navbar'); ?>
 <?php loadPartial('showcase-search'); ?>
 <?php loadPartial('top-banner'); ?>
-  
+
+
 <!-- Job Listings -->
 <section>
   <div class="container mx-auto p-4 mt-4">
@@ -31,10 +32,10 @@
             Details
           </a>
         </div>
-      </div>  
+      </div>
 
-         <!-- Job Listing 2: Marketing Specialist -->
-         <div class="rounded-lg shadow-md bg-white">
+      <!-- Job Listing 2: Marketing Specialist -->
+      <div class="rounded-lg shadow-md bg-white">
         <div class="p-4">
           <h2 class="text-xl font-semibold">Marketing Specialist</h2>
           <p class="text-gray-700 text-lg mt-2">
@@ -56,10 +57,10 @@
             Details
           </a>
         </div>
-      </div>       
+      </div>
 
-       <!-- Job Listing 3: Web Developer -->
-       <div class="rounded-lg shadow-md bg-white">
+      <!-- Job Listing 3: Web Developer -->
+      <div class="rounded-lg shadow-md bg-white">
         <div class="p-4">
           <h2 class="text-xl font-semibold">Web Developer</h2>
           <p class="text-gray-700 text-lg mt-2">
@@ -80,9 +81,11 @@
           <a href="details.html" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
             Details
           </a>
-    
-           <!-- Job Listing 4: Data Analyst -->
-           <div class="rounded-lg shadow-md bg-white">
+        </div>
+      </div>
+
+      <!-- Job Listing 4: Data Analyst -->
+      <div class="rounded-lg shadow-md bg-white">
         <div class="p-4">
           <h2 class="text-xl font-semibold">Data Analyst</h2>
           <p class="text-gray-700 text-lg mt-2">
@@ -104,8 +107,9 @@
             Details
           </a>
         </div>
-      </div>     
-        <!-- Job Listing 5: Graphic Designer -->
+      </div>
+
+      <!-- Job Listing 5: Graphic Designer -->
       <div class="rounded-lg shadow-md bg-white">
         <div class="p-4">
           <h2 class="text-xl font-semibold">Graphic Designer</h2>
@@ -127,12 +131,10 @@
           <a href="details.html" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
             Details
           </a>
-          
-          </div>
         </div>
-        
+      </div>
 
-        <div class="rounded-lg shadow-md bg-white">
+      <div class="rounded-lg shadow-md bg-white">
         <div class="p-4">
           <h2 class="text-xl font-semibold">Data Scientist</h2>
           <p class="text-gray-700 text-lg mt-2">
@@ -154,14 +156,15 @@
             Details
           </a>
         </div>
-        </div>
-        
       </div>
-      <a href="listings.html" class="block text-xl text-center">
+    </div>
+    <a href="listings.html" class="block text-xl text-center">
       <i class="fa fa-arrow-alt-circle-right"></i>
       Show All Jobs
     </a>
 </section>
-<?php loadPartial('bottom-banner'); ?>
-<?php loadPartial('footer'); ?>     
 
+
+
+<?php loadPartial('bottom-banner'); ?>
+<?php loadPartial('footer'); ?>

a765be0540fec7167d64a7e40ee9ac62e4e7cfd1
bbyte47 <gregg@bari-flora.com>
2024-04-09 11:52:08 -0400

    Created views, seperate router files and added classes

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,6 +1,5 @@
 <?= loadPartial('head') ?>
 <?= loadPartial('navbar') ?>
-<?= loadPartial('showcase-search') ?>
 <?= loadPartial('top-banner') ?>
 
 
@@ -158,11 +157,9 @@
         </div>
       </div>
     </div>
-    <a href="/listings" class="block text-xl text-center">
-      <i class="fa fa-arrow-alt-circle-right"></i>
-      Show All Jobs
-    </a>
 </section>
+
+
 
 <?= loadPartial('bottom-banner') ?>
 <?= loadPartial('footer') ?>

bc0978be25e674aee4ae7042c9440dea29097e52
bbyte47 <gregg@bari-flora.com>
2024-04-23 12:27:38 -0400

    Just before refactor for namespaces and autoloading

--- helpers.php
+++ helpers.php
@@ -18,7 +18,7 @@ function basePath($path = '')
  * @return void
  * 
  */
-function loadView($name)
+function loadView($name, $data = [])
 {
   $viewPath = basePath("views/{$name}.view.php");
 
@@ -26,6 +26,7 @@ function loadView($name)
   // inspect($viewPath);
 
   if (file_exists($viewPath)) {
+    extract($data);
     require $viewPath;
   } else {
     echo "View '{$name} not found!'";
@@ -75,4 +76,15 @@ function inspectAndDie($value)
   echo '<pre>';
   die(var_dump($value));
   echo '</pre>';
+}
+
+/**
+ * Format salary
+ * 
+ * @param string $salary
+ * @return string Formatted Salary
+ */
+function formatSalary($salary)
+{
+  return '$' . number_format(floatval($salary));
 }

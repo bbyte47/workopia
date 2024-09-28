34841b22136579ea276dc76f656002a4efe6c9cc
bbyte47 <gregg@bari-flora.com>
2024-09-28 10:53:12 -0400

    First Commit up to getting database data in home page

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../helpers.php';
+require basePath('Router.php');
+require basePath('Database.php');
+
+// Instatiate the router
+$router = new Router();
+
+// Get routes
+$routes = require basePath('routes.php');
+
+// Get current URI and HTTP method
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+// inspectAndDie($uri);
+
+// Route the request
+$router->route($uri, $method);
+
+?>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Workopia
+
+Workopia is a job listing website that is taken from a Udemy course PHP 2024 with part of the course being project based. This app is the project and one of the interesting aspects is it creating a framework that while similar to Laravel or Symphony, it is much simplier and doesn't have nearly the bells and whistles that those do. 
+
+Will update this file as I progress through the course.

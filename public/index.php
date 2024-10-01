8613bc418308a19a5775eb3d86c68adcc72c7bb8
bbyte47 <gregg@bari-flora.com>
2024-10-01 17:32:18 -0400

    Refactor router for params

--- public/index.php
+++ public/index.php
@@ -13,11 +13,11 @@ $routes = require basePath('routes.php');
 
 // Get current URI and HTTP method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+
 
 // inspectAndDie($uri);
 
 // Route the request
-$router->route($uri, $method);
+$router->route($uri);
 
 ?>
